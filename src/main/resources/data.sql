e4faf170db0d93f872420f541e0d72e2f8586bcc
Vladyslav Moskalenko <vladkiew15@gmail.com>
2023-11-25 00:17:24 +0100

    tesdt

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
-CREATE TABLE IF NOT EXISTS posts (
-                                     id SERIAL PRIMARY KEY,
-                                     description TEXT,
-                                     content TEXT,
-                                     title VARCHAR(255)
+CREATE TABLE IF NOT EXISTS category (
+                                        id BIGSERIAL PRIMARY KEY,
+                                        name VARCHAR(255) NOT NULL,
+    description TEXT
     );
 
-INSERT INTO posts (description, content, title)
-VALUES ('Example Description', 'Example Content', 'Example Title');
+INSERT INTO category (name, description) VALUES ('Technology', 'Posts related to technology') ON CONFLICT (name) DO NOTHING;
+INSERT INTO category (name, description) VALUES ('Lifestyle', 'Posts related to lifestyle') ON CONFLICT (name) DO NOTHING;
+-- Add more INSERT statements as needed for other categories.
