4b61611a2e6cb758f7af85664cffc3737a82b8d0
Vladyslav Moskalenko <vladkiew15@gmail.com>
2023-11-24 23:50:54 +0100

    tesdt

--- init.sql/init-db.sql
+++ init.sql/init-db.sql
@@ -1,2 +1,9 @@
+CREATE TABLE IF NOT EXISTS posts (
+                                     id SERIAL PRIMARY KEY,
+                                     description TEXT,
+                                     content TEXT,
+                                     title VARCHAR(255)
+    );
+
 INSERT INTO posts (description, content, title)
 VALUES ('Example Description', 'Example Content', 'Example Title');
