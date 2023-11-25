bdbd7531a8c9cf6c8ec61d4666b6b01cae3519ff
Vladyslav Moskalenko <vladkiew15@gmail.com>
2023-11-25 10:01:16 +0100

    tesdt

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,12 @@
-CREATE TABLE IF NOT EXISTS category (
-                                        id BIGSERIAL PRIMARY KEY,
-                                        name VARCHAR(255) NOT NULL,
-    description TEXT
-    );
+CREATE TABLE if not exists employee (
+                          id SERIAL PRIMARY KEY,
+                          employee_name VARCHAR(45),
+                          salary VARCHAR(45) NOT NULL,
+                          created_at TIMESTAMP NOT NULL,
+                          updated_at TIMESTAMP DEFAULT NULL
+);
 
-INSERT INTO category (name, description) VALUES ('Technology', 'Posts related to technology') ON CONFLICT (name) DO NOTHING;
-INSERT INTO category (name, description) VALUES ('Lifestyle', 'Posts related to lifestyle') ON CONFLICT (name) DO NOTHING;
--- Add more INSERT statements as needed for other categories.
+INSERT INTO employee (employee_name, salary, created_at, updated_at)
+VALUES ('Иван Иванов', '50000', '2023-01-01 09:00:00', NULL),
+       ('Елена Петрова', '55000', '2023-01-02 10:00:00', '2023-03-01 12:00:00'),
+       ('Алексей Смирнов', '60000', '2023-01-03 11:00:00', NULL);
